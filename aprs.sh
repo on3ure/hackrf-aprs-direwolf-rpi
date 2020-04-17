47f5a04323f744e97467bbd15f10b2291bd2dd2a
Joeri Van Dooren <ure@on3ure.be>
2020-04-17 16:58:14 +0200

    fix

--- aprs.sh
+++ aprs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+SoapySDRUtil --probe="driver=hackrf" > /dev/null 2>&1
+
 /usr/local/bin/rx_fm -M fm -f 28.800M -s 24000 - | /usr/local/bin/direwolf -c /aprs/sdr-1200bps.conf -l /var/log/direwolf.log -r 24000 -D 1 -B 1200 - >/dev/tty3
