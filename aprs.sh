ad942fb27d8243f6568b704cca07da5b924e0acd
Joeri van Dooren <joeri@weepee.org>
2020-09-07 20:27:57 +0200

    Update aprs.sh
    auto gain control

--- aprs.sh
+++ aprs.sh
@@ -2,4 +2,4 @@
 
 SoapySDRUtil --probe="driver=hackrf" > /dev/null 2>&1
 
-/usr/local/bin/rx_fm -M fm -f 28.800M -s 24000 - | /usr/local/bin/direwolf -c /aprs/sdr-1200bps.conf -l /var/log/direwolf -r 24000 -D 1 -B 1200 - >/dev/tty3
+/usr/local/bin/rx_fm -E AGC -M fm -f 28.800M -s 24000 - | /usr/local/bin/direwolf -c /aprs/sdr-1200bps.conf -l /var/log/direwolf -r 24000 -D 1 -B 1200 - >/dev/tty3
